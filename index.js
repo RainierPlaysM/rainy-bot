017c708c16dd16ecf8d054b3953511d039d6e7e6
rainplaysmineyt <rainplaysmineyt@gmail.com>
2020-12-13 14:53:08 +0800

    a

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const config = require('./config.json');
 
 client.on('ready', () => {
     console.log('The bot is online');
+
+    command(client, ['ping', 'test'], message => {
+      message.channel.send('Poggers!') 
+    })
+
+    command(client, ['servers'], message => {
+        client.guilds.cache.forEach((guild) => {
+            message.channel.send(`${guild.name} has a total of ${guild.memberCount} members.`)
+        }) 
+    })
 })
 
 client.login(config.token)
