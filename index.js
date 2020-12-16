724fe5d3c077188d32e562f27ac7a79d209f13b3
rainplaysmineyt <rainplaysmineyt@gmail.com>
2020-12-16 17:15:12 +0800

    the main part of the bot | v.1.0.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client()
 
 const command = require('./commands.js')
-const config = require('./config.json');
+// const config = require('./config.json');
 
 client.on('ready', () => {
     console.log('The bot is online');
